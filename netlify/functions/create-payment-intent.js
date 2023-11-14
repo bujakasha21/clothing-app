fa9ea180064d92d6aaa4ffb6118b06ada13034b8
bujakasha21 <bujakdavor21@gmail.com>
2023-11-14 22:12:35 +0100

    app finished

--- netlify/functions/create-payment-intent.js
+++ netlify/functions/create-payment-intent.js
@@ -2,9 +2,9 @@ require("dotenv").config();
 
 const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
 
-exports.handler = async (e) => {
+exports.handler = async (event) => {
   try {
-    const { amount } = JSON.parse(e.body);
+    const { amount } = JSON.parse(event.body);
 
     const paymentIntent = await stripe.paymentIntents.create({
       amount,
--- src/components/button/button.component.jsx
+++ src/components/button/button.component.jsx
@@ -3,6 +3,7 @@ import {
   BaseButton,
   GoogleSignInButton,
   InvertedSignInButton,
+  ButtonSpinner,
 } from "./button.stlyes";
 
 export const BUTTON_TYPE_CLASSES = {
@@ -18,9 +19,13 @@ const getButton = (buttonType = BUTTON_TYPE_CLASSES.base) =>
     [BUTTON_TYPE_CLASSES.inverted]: InvertedSignInButton,
   }[buttonType]);
 
-const Button = ({ children, buttonType, ...otherProps }) => {
+const Button = ({ children, buttonType, isLoading, ...otherProps }) => {
   const CustomButton = getButton(buttonType);
-  return <CustomButton {...otherProps}>{children}</CustomButton>;
+  return (
+    <CustomButton disabled={isLoading} {...otherProps}>
+      {isLoading ? <ButtonSpinner /> : children}
+    </CustomButton>
+  );
 };
 
 export default Button;
